e59800071aed39f61f2e3c1e9fb5bc573d45ee3d
Vasily Lavrov <29576033+xv2@users.noreply.github.com>
2020-12-13 12:48:55 +0300

    run prettier

--- __tests__/functional/best/page-model.js
+++ __tests__/functional/best/page-model.js
@@ -1,20 +1,20 @@
 import { By } from 'selenium-webdriver';
 
-export const getPageModel = async driver => {
-  const getChain = element => ({
+export const getPageModel = async (driver) => {
+  const getChain = (element) => ({
     getText: () => element.getText(),
-    sendKeys: keys => element.sendKeys(keys)
+    sendKeys: (keys) => element.sendKeys(keys),
   });
 
   const draftModel = {
     emailInput: '//*[@id="email"]',
     passwordInput: '//*/input[2]',
     checkSumDiv: '//*/div[4]',
-    loginButton: '//*/button'
+    loginButton: '//*/button',
   };
 
   const keys = Object.keys(draftModel);
-  const promises = keys.map(key => driver.findElement(By.xpath(draftModel[key])));
+  const promises = keys.map((key) => driver.findElement(By.xpath(draftModel[key])));
   const elements = await Promise.all(promises);
 
   return keys.reduce((acc, key, index) => ({ ...acc, [key]: getChain(elements[index]) }), {});
